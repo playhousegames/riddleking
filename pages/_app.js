73603eaf89c2e67c15c7ab68030cd513373671d0
playhousegames <playhousegamesuk@gmail.com>
2026-05-09 15:57:06 +0100

    Fix favicon — point to logo-icon.png, add apple-touch-icon

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/logo-icon.png" />
+        <link rel="apple-touch-icon" href="/logo-icon.png" />
       </Head>
 
       {/*
